72b40c9a59aefdab02df3c24945799f54d175fde
Randy Lujano <randylujano@gmail.com>
2024-07-26 16:58:10 -0700

    Update GPS_Position.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GPS_Position.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GPS_Position.java
@@ -1,95 +1,94 @@
 package org.firstinspires.ftc.teamcode;
-import static androidx.core.content.ContextCompat.getSystemService;
 
-import android.content.Context;
-import android.util.Size;
-
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.hardware.HardwareDeviceManager;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.exception.RobotCoreException;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.robotcore.hardware.usb.RobotUsbDevice;
-import com.qualcomm.robotcore.hardware.usb.serial.SerialPort;
 
-import com.qualcomm.robotcore.hardware.usb.RobotUsbManager;
-import com.qualcomm.robotcore.hardware.usb.serial.RobotUsbDeviceTty;
+import com.qualcomm.robotcore.hardware.DeviceManager;
+import com.qualcomm.robotcore.hardware.ScannedDevices;
 import com.qualcomm.robotcore.util.SerialNumber;
-import java.util.List;
+
+import java.util.Set;
+
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-
+import com.qualcomm.robotcore.hardware.usb.serial.RobotUsbManagerTty;
 import com.qualcomm.robotcore.hardware.usb.RobotUsbDeviceImplBase;
-import com.qualcomm.robotcore.hardware.usb.RobotUsbManagerCombining;
-import android.hardware.usb.UsbDevice;
-import android.hardware.usb.UsbManager;
-import android.content.Context;
-
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
 
 
 @TeleOp(name = "GPS_Testing", group = "Senor")
 //@Disabled
 public class GPS_Position extends LinearOpMode
 {
-   RobotUsbManagerCombining managerCombining = new RobotUsbManagerCombining();
-   List<SerialNumber> serialNumbers;
-   RobotUsbDeviceImplBase gpsSensor;
-   int productId;
-   int vendorId;
-   int bcdDevice;
 
 
-    public List<SerialNumber> findDevices() throws RobotCoreException {
-        return managerCombining.scanForDevices();
-    }
 
-    public void connectToGPS(int i) throws RobotCoreException {
-        serialNumbers = findDevices();
-        SerialNumber dev = serialNumbers.get(i);
-        RobotUsbDevice gps = managerCombining.openBySerialNumber(dev);
-        RobotUsbDevice.USBIdentifiers identifiers = gps.getUsbIdentifiers();
-        productId = identifiers.productId;
-        vendorId = identifiers.vendorId;
-        bcdDevice = identifiers.bcdDevice;
+    HardwareDeviceManager DevManager = new HardwareDeviceManager(AppUtil.getDefContext(),null);
+    ScannedDevices Devices = null;
+    Set<SerialNumber> USB_Keys;
+    SerialNumber[] USB_Devices;
+    SerialNumber GPS_Serial_Num;
+    RobotUsbManagerTty Tty_Manager = new RobotUsbManagerTty();
+    DeviceManager.UsbDeviceType Dev_type;
+//    public List<SerialNumber> findDevices() throws RobotCoreException {
+//        return deviceMan.scanForDevices();
+//    }
 
-//        for (SerialNumber i : serialNumbers) {
-//            RobotUsbDevice gps = managerCombining.openBySerialNumber(i);
-//            RobotUsbDevice.USBIdentifiers identifiers = gps.getUsbIdentifiers();
-//            productId = identifiers.productId;
-//            vendorId = identifiers.vendorId;
-//            bcdDevice = identifiers.bcdDevice;
-//        }
-    }
+//    public void connectToGPS(int i) throws RobotCoreException {
+//        serialNumbers = findDevices();
+//        SerialNumber dev = serialNumbers.get(i);
+////        RobotUsbDevice gps = DeviceManager.openBySerialNumber(dev);
+////        RobotUsbDevice.USBIdentifiers identifiers = gps.getUsbIdentifiers();
+////        productId = identifiers.productId;
+////        vendorId = identifiers.vendorId;
+////        bcdDevice = identifiers.bcdDevice;
+//
+////        for (SerialNumber i : serialNumbers) {
+////            RobotUsbDevice gps = managerCombining.openBySerialNumber(i);
+////            RobotUsbDevice.USBIdentifiers identifiers = gps.getUsbIdentifiers();
+////            productId = identifiers.productId;
+////            vendorId = identifiers.vendorId;
+////            bcdDevice = identifiers.bcdDevice;
+////        }
+//    }
 
-    public void runOpMode()
-    {
+
+    public void runOpMode() {
         telemetry.addData(">>", "Press start to continue");
-
-
-
         telemetry.update();
-
-        try {
-            serialNumbers = managerCombining.scanForDevices();
-        } catch (RobotCoreException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            connectToGPS(0);
-        } catch (RobotCoreException e) {
-            throw new RuntimeException(e);
-        }
-
         waitForStart();
+
+        try {
+            Devices = DevManager.scanForUsbDevices();
+        } catch (RobotCoreException e) {
+            throw new RuntimeException(e);
+        }
+        USB_Keys = Devices.keySet();
+        USB_Devices = (SerialNumber[]) USB_Keys.toArray(new SerialNumber[0]);
+        for (SerialNumber usbDevice : USB_Devices) {
+            if (!usbDevice.isEmbedded()) {
+                Dev_type = RobotUsbDeviceImplBase.getDeviceType(usbDevice);
+            }
+        }
+
 
 
 
         while(opModeIsActive())
         {
-            telemetry.addData("List Size:", serialNumbers.size());
-            telemetry.addData("Serial Number:", serialNumbers.get(0).getString());
-            telemetry.addData("product id: ", productId);
-            telemetry.addData("vendor id: ", vendorId);
-            telemetry.addData("bcdDevice: ", bcdDevice);
+            if (USB_Devices.length <= 1) {
+                telemetry.addData("Hardware Error", "No USB Device Connected");
+            } else{
+                telemetry.addData("USB Device Map", Devices.toSerializationString());
+                telemetry.addData("# of USB devices Connected", USB_Devices.length-1);
+                for (SerialNumber devices : USB_Devices) {
+                    if (!devices.isEmbedded()) {
+                        telemetry.addData("Serial Number", devices.toString());
+                        telemetry.addData("Device Type:", Dev_type.toString());
+                    }
+                }
+
+            }
             telemetry.update();
         }
     }
