778166c583e643324bbb379e40537c9a18e71257
Randy Lujano <randylujano@gmail.com>
2024-08-05 02:55:37 -0700

    adding gps data

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_GPS_Device.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_GPS_Device.java
@@ -3,25 +3,43 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+
 @TeleOp(name = "Test GPS", group = "Odom")
 public class Test_GPS_Device extends LinearOpMode {
 
+
     @Override
     public void runOpMode() throws InterruptedException {
+
         telemetry.addData("Test Ports" +
                 " >>", "Press start to continue");
         telemetry.update();
         waitForStart();
         VEX_GPS_Sensor GPS = new VEX_GPS_Sensor(0,0,0);
         while(opModeIsActive()) {
-            GPS.getData();
-            telemetry.addData("X",GPS.X);
-            telemetry.addData("Y",GPS.Y);
-            telemetry.addData("Azimuth ",GPS.Az);
-            telemetry.addData("Elevation",GPS.El);
-            telemetry.addData("Rotation",GPS.Rot);
-            telemetry.update();
+            if(GPS.isConnected) {
+                telemetry.addData("Vex GPS Sensor is connected","");
+                GPS.getData();
+                telemetry.addData("Status",GPS.Status);
+                telemetry.addData("X", GPS.X);
+                telemetry.addData("Y", GPS.Y);
+                telemetry.addData("Azimuth ", GPS.Az);
+                telemetry.addData("Elevation", GPS.El);
+                telemetry.addData("Rotation", GPS.Rot);
 
+                telemetry.addData("Buffer Raw Data", "");
+                for (int i = 0; i < GPS.gps_Buffer.length; i++) {
+                    telemetry.addData("     Byte " + i, GPS.gps_Buffer[i]);
+                }
+            }
+            else{
+                telemetry.addData("Vex GPS Sensor is disconnected","");
+            }
+            telemetry.update();
+        }
+        if(!opModeIsActive())
+        {
+            GPS.Close_GPS();
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VEX_GPS_Sensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VEX_GPS_Sensor.java
@@ -12,6 +12,8 @@ import com.hoho.android.usbserial.driver.UsbSerialPort;
 import com.hoho.android.usbserial.driver.UsbSerialProber;
 import com.hoho.android.usbserial.util.SerialInputOutputManager;
 
+
+
 import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
 
 import java.io.IOException;
@@ -26,10 +28,11 @@ public class VEX_GPS_Sensor {
     UsbManager manager;
     UsbDevice gps_Device = null;
     UsbSerialPort gps_Port = null;
-    byte[] data = new byte[16];
-    protected float X_Offset = 0;
-    protected float Y_Offset = 0;
-    protected float Angle_Offset = 0;
+    byte[] gps_Buffer = new byte[16];
+    protected float X_Offset ;
+    protected float Y_Offset ;
+    protected float Angle_Offset ;
+    public int Status;
     public double X = 0;
     public double Y = 0;
     public double Az = 0;
@@ -65,6 +68,7 @@ public class VEX_GPS_Sensor {
         }
     }
 
+
     private void Connect_GPS() {
         if (gps_Device != null) {
             UsbDeviceConnection gps_Conn = manager.openDevice(gps_Device);
@@ -76,6 +80,7 @@ public class VEX_GPS_Sensor {
             try {
                 gps_Port.open(gps_Conn);
                 gps_Port.setParameters(gps_baudrate, UsbSerialPort.DATABITS_8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+                gps_Port.setBreak(false);
                 gps_Port.setDTR(true);
             } catch (IOException e) {
                 throw new RuntimeException("GPS Sensor is connected but can't open a connection try power cycling via the USB cable", e);
@@ -83,23 +88,32 @@ public class VEX_GPS_Sensor {
             isConnected = true;
             SerialInputOutputManager serIoManager = new SerialInputOutputManager(gps_Port);
             serIoManager.start();
+
+        }
+    }
+
+    public void Close_GPS(){
+        try {
+            gps_Port.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Could not close GPS port", e);
         }
     }
 
     public void getData() {
         try {
-            gps_Port.read(data, 16, 10);
+            gps_Port.read(gps_Buffer, 16, 10);
         } catch (IOException e) {
             throw new RuntimeException("GPS cant read data from virtual Com", e);
         }
         if (gps_Port.isOpen()) {
-            int status = data[1];
-            X = Float.parseFloat(data[2] + "." + data[3]);
-            Y = Float.parseFloat(data[4] + "." + data[5]);
-            Az = Float.parseFloat(data[8] + "." + data[9]);
-            El = Float.parseFloat(data[10] + "." + data[11]);
-            Rot = Float.parseFloat(data[12] + "." + data[13]);
-            if (status == 20) {
+            Status = gps_Buffer[1];
+            X = Float.parseFloat(gps_Buffer[2] + "." + gps_Buffer[3]);
+            Y = Float.parseFloat(gps_Buffer[4] + "." + gps_Buffer[5]);
+            Az = Float.parseFloat(gps_Buffer[8] + "." + gps_Buffer[9]);
+            El = Float.parseFloat(gps_Buffer[10] + "." + gps_Buffer[11]);
+            Rot = Float.parseFloat(gps_Buffer[12] + "." + gps_Buffer[13]);
+            if (Status == 20) {
                 X = X * 1000;
                 Y = Y * 1000;
                 Az = ((Az / 32768.0 * 180.0) - Angle_Offset) % 360;
